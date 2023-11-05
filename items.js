5fa730ee45de3d60559b84af05fe097ee19d2f70
JoelSngZhenWei <joelsngzw@gmail.com>
2023-11-06 02:59:49 +0800

    Added theme buttons

---
+++ items.js
@@ -0,0 +1,63 @@
+const items=
+[
+    {
+        "name": "Apples",
+        "cost_per_unit": 1.99,
+        "farm_of_origin": "Open Farm Community",
+        "image_link": "placeholder_image_link_1.jpg"
+    },
+    {
+        "name": "Carrots",
+        "cost_per_unit": 0.99,
+        "farm_of_origin": "Kok Fah Technology Farm Pte Ltd",
+        "image_link": "placeholder_image_link_2.jpg"
+    },
+    {
+        "name": "Tomatoes",
+        "cost_per_unit": 2.49,
+        "farm_of origin": "Citizen Farm",
+        "image_link": "placeholder_image_link_3.jpg"
+    },
+    {
+        "name": "Eggs",
+        "cost_per_unit": 3.99,
+        "farm_of_origin": "Farmart Centre",
+        "image_link": "placeholder_image_link_4.jpg"
+    },
+    {
+        "name": "Milk",
+        "cost_per_unit": 2.79,
+        "farm_of_origin": "Viknesh Dairy Farm",
+        "image_link": "placeholder_image_link_5.jpg"
+    },
+    {
+        "name": "Lettuce",
+        "cost_per_unit": 1.49,
+        "farm_of_origin": "Open Farm Community",
+        "image_link": "placeholder_image_link_6.jpg"
+    },
+    {
+        "name": "Chicken",
+        "cost_per_unit": 5.99,
+        "farm_of_origin": "BEE AMAZED GARDEN",
+        "image_link": "placeholder_image_link_7.jpg"
+    },
+    {
+        "name": "Potatoes",
+        "cost_per_unit": 1.29,
+        "farm_of_origin": "Kok Fah Technology Farm Pte Ltd",
+        "image_link": "placeholder_image_link_8.jpg"
+    },
+    {
+        "name": "Strawberries",
+        "cost_per_unit": 3.99,
+        "farm_of_origin": "Open Farm Community",
+        "image_link": "placeholder_image_link_9.jpg"
+    },
+    {
+        "name": "Fish",
+        "cost_per_unit": 7.99,
+        "farm_of_origin": "Qian Hu Fish Farm",
+        "image_link": "placeholder_image_link_10.jpg"
+    }
+]
