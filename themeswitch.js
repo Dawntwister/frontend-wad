ff61617873319294d82a450c59bfde22dc5f7501
JoelSngZhenWei <joelsngzw@gmail.com>
2023-11-06 16:21:48 +0800

    Increase functionality of theme
    Edited such that theme carries over between pages

--- themeswitch.js
+++ themeswitch.js
@@ -1,13 +1,23 @@
 $(document).ready(function () {
+    // Retrieve the theme preference from localStorage
+    const savedTheme = localStorage.getItem('theme');
+    
+    // Set the theme based on the saved preference (or default to "light" if not set)
+    $("html").attr("data-bs-theme", savedTheme || "light");
+
     // Handle the Light Theme selection
     $("#lightTheme").click(function () {
         // Set the data-bs-theme attribute to "light"
         $("html").attr("data-bs-theme", "light");
+        // Store the theme preference in localStorage
+        localStorage.setItem('theme', 'light');
     });
 
     // Handle the Dark Theme selection
     $("#darkTheme").click(function () {
         // Set the data-bs-theme attribute to "dark"
         $("html").attr("data-bs-theme", "dark");
+        // Store the theme preference in localStorage
+        localStorage.setItem('theme', 'dark');
     });
 });
