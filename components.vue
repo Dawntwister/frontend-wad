d211c2bbe07a0e0bcf36c7ab9e9aff3d385428f7
JoelSngZhenWei <joelsngzw@gmail.com>
2023-11-08 04:56:39 +0800

    fixed broken components

--- components.vue
+++ components.vue
@@ -25,7 +25,7 @@ root.component("fg_top_green",{
         <div class="py-1 greencontainer">
         <div class="d-flex flex-wrap align-items-center">
             <!-- Logo Column on Small Viewport -->
-            <div class="col-12 col-md-7 mb-2 mb-md-0 text-center text-md-start">
+            <div class="col-12 col-md-7 mb-2 mb-md-0 text-center">
                 <a href="../home/home.html" class=" justify-content-center justify-content-md-start text-decoration-none">
                     <img src="../images/logo/logo.png" alt="" class="w-25" >
                 </a>
@@ -50,10 +50,10 @@ root.component("fg_top_green",{
                     });
                 </script>
                 <!-- End theme selection script -->
-            <div class="col-12 col-md-5 pe-3 text-truncate">
+            <div class="col-12 col-md-5 pe-3">
                 <ul class="nav text-small justify-content-center justify-content-md-end">
                     <li id="profilebutton">
-                        <a href="../Login Signup Page/login.html" class="nav-link nav-top">
+                        <a href="#" class="nav-link nav-top">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi d-block mx-auto mb-1" viewBox="0 0 16 16">
                                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                             </svg>
