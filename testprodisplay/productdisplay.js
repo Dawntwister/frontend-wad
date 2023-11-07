b7780738222750b43a7dbc3dc0dd62f18a5aa9d0
aaronwky <aaron.wong.2021@scis.smu.edu.sg>
2023-11-08 04:55:32 +0800

    add to cart page added but not linked

---
+++ testprodisplay/productdisplay.js
@@ -0,0 +1,14 @@
+const cart = [];
+
+function addToCart(product) {
+    const existingProduct = cart.find((p) => p.id === product.id);
+
+    if (existingProduct) {
+        existingProduct.quantity++;
+    } else {
+        cart.push({ ...product, quantity: 1 });
+    }
+
+    // Save the cart to local storage
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
