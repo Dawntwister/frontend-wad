703cee87009cf62673c74f63667ea05ddb0b54ff
aaronwky <aaron.wong.2021@scis.smu.edu.sg>
2023-11-08 07:21:39 +0800

    looping cart to empty cart pls help

--- Product/productdisplay.js
+++ Product/productdisplay.js
@@ -12,3 +12,23 @@ function addToCart(product) {
     // Save the cart to local storage
     localStorage.setItem('cart', JSON.stringify(cart));
 }
+
+const clearCartButton = document.getElementById('clear-cart-button');
+
+    // Add a click event listener to the button
+    clearCartButton.addEventListener('click', function () {
+        // Clear the cart items from local storage
+        localStorage.removeItem('cart');
+
+        // Clear the cart display on the page
+        cartItems.innerHTML = ''; // Clear the table
+
+        // Reset the grand total values
+        grandTotalCarbonFootprint.textContent = '';
+        grandTotalWaste.textContent = '';
+        grandTotalWater.textContent = '';
+
+        // You can also display a message to inform the user that the cart has been cleared
+        // For example:
+        // alert('Cart has been cleared!');
+    });
