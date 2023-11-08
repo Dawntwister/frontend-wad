22fd292c91a661ae0039b04d7e26f500e05bb423
JoelSngZhenWei <joelsngzw@gmail.com>
2023-11-08 22:30:33 +0800

    asdas

--- function_paypal/index.js
+++ function_paypal/index.js
@@ -17,3 +17,32 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+
+exports.createStripeCheckout = functions.https.onCall(async(data, context)=>{
+    const stripe = require("stripe")(functions.config().stripe.secret_key);
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        mode:"payment",
+        success_url:"",
+        cancel_url:"",
+        line_items:[
+            {
+                quantity:1,
+                price_data:{
+                    currency:"usd",
+                    unit_amount:(100)*100,
+                    product_data:{
+                        name:"New camera"
+                    },
+                },
+            },
+        ],
+    });
+
+
+    return{
+        id:session.id,
+    };
+});
